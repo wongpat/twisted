8033dd92eecce864172735c76aed66c520d2dbf9
Tom Most <twm@freecog.net>
2021-01-12 21:28:57 -0800

    Allow @inlineCallbacks on coroutine functions

--- src/twisted/internet/defer.py
+++ src/twisted/internet/defer.py
@@ -1634,9 +1634,9 @@ def inlineCallbacks(f):
                 "inlineCallbacks requires %r to produce a generator; instead"
                 "caught returnValue being used in a non-generator" % (f,)
             )
-        if not isinstance(gen, types.GeneratorType):
+        if not isinstance(gen, types.GeneratorType) and not iscoroutine(gen):
             raise TypeError(
-                "inlineCallbacks requires %r to produce a generator; "
+                "inlineCallbacks requires %r to produce a generator or coroutine; "
                 "instead got %r" % (f, gen)
             )
         return _cancellableInlineCallbacks(gen)
